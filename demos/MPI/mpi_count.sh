cfb5614c5b75301faf735432089c85979a89f3fb
bbarker <beb82@cornell.edu>
2015-05-10 19:33:38 -0500

    added DMTCP scripting to sbatch example

--- demos/MPI/mpi_count.sh
+++ demos/MPI/mpi_count.sh
@@ -3,9 +3,10 @@
 #SBATCH -J myMPI          # Job Name
 #SBATCH -o myMPI.o%j      # Name of the output file (myMPI.oJobID)
 #SBATCH -p development    # Queue name
-#SBATCH -t 00:00:08       # Run time (hh:mm:ss) - 8 seconds
-#SBATCH -n 20             # 4 tasks total
+#SBATCH -t 00:00:45       # Run time (hh:mm:ss) - 45 seconds
+#SBATCH -n 4              # 4 tasks total
+#SBATCH -N 2              # run on 2 nodes
 #SBATCH -e myMPI.err%j    # Direct error to the error file
 #SBATCH -A TG-STA110019S  # Account number (replace with yours)
 
-ibrun dmtcp_launch -i 4 ./mpi_count
+ibrun ./mpi_count
